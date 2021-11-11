dd6371f58ff8bb3397c98cf9caabbd0edbc97f7a
Vignesh-Seethapathy <vignesh.seethapathy@gmail.com>
2021-11-11 08:52:01 +0530

    revert previous commit

--- assets/js/lunr-store.js
+++ assets/js/lunr-store.js
@@ -2,11 +2,11 @@
 # create lunr store 
 ---
 var store = [ 
-    {% for item in site.html_pages %} 
+    {% assign content_pages = site.html_pages | where_exp: 'p','p.searchable != false' %}
+    {% for item in content_pages %}
     { 
         "url": {{ item.url | relative_url | jsonify }},
         "title": {{ item.title | jsonify }},
-        "topics": {{ item.topics | jsonify }},
         "text": {{ item.content | strip_html | normalize_whitespace | jsonify }}
     }{%- unless forloop.last -%},{%- endunless -%}
     {%- endfor -%}
